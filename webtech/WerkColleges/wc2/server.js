6d4f443499c567d7184afc8a6984c4886e218071
Csanád <bakoscsanad@gmail.com>
2018-12-03 08:50:12 +0100

    wc2 added

---
+++ webtech/WerkColleges/wc2/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const cookies = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+// 4. and 5. Data structures to support the application
+
+// 6. Count current number of users to identify them
+
+// 2. Create Express server
+// serve static files if available
+// use body-parser to parse parameters in the body of POST requests
+
+// 3. Log each request received by the server
+
+// 16. Check for the cookie in the request. If there is one, it identifies the user.
+// If there is not, create a new userId and set the cookie to identify him/her
+
+// 7. and 8. Get list of suggestions
+
+// 14. Post a new suggestion
+
+// 10. and 18. Upvote a suggestion
+
+// 11. and 18. Downvote a suggestion
+
+// 2. Create HTTP server and listen
+
