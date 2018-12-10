a7fdfecef4299d5d18e829ab14648c09d9eac0ce
Csanád <bakoscsanad@gmail.com>
2018-12-10 13:36:37 +0100

    user identification with cookies added

--- webtech/WerkColleges/wc2/server.js
+++ webtech/WerkColleges/wc2/server.js
@@ -33,10 +33,29 @@ app.use(function(req, res, next) {
 
 // 16. Check for the cookie in the request. If there is one, it identifies the user.
 // If there is not, create a new userId and set the cookie to identify him/her
+app.use(cookies("mysecret")); // this will encrypt cookies to avoid users tampering with them
+app.use(function(req, res, next){
+	var userId = req.signedCookies.userId;
+	if (userId === undefined) { //no cookie
+		userId = ++usersCount;
+
+		console.log("# Setting new cookie for user " + userId);
+		res.cookie("userId", userId, {signed: true, httpOnly: true});
+
+
+	}
+
+	req.userId = parseInt(userId); // store the parsed userId for the next components
+	next(); // call on the next component
+
+
+
+});
+
 
 // 7. and 8. Get list of suggestions
 app.get("/suggestions/", function(req, res) {
-    var userId = 2; // for now
+    var userId = req.userId;
     // process list of suggestions, counting upvotes and checking whether this user is the owner
     var result = [];
     for(var i = 0; i < suggestions.length; i++) {
@@ -58,7 +77,7 @@ app.use(bodyParser.json());
 app.post("/suggestions/", function(req, res){
 	var message = req.body.message;
 	var nick = req.body.nick;
-	var userId = 2; //for now
+	var userId = req.userId;
 
 	var s = {userId: userId, nick: nick, message: message, voters: []};
 	suggestions.push(s);
@@ -74,7 +93,7 @@ app.post("/suggestions/", function(req, res){
 app.post("/votes/:suggestion", function(req, res) {
 	var sugId = req.params.suggestion; //suggesition id is encoded in url path 
 	// the : spcifies that there is a parameter
-	var userId = 2; //for now
+	var userId = req.userId;
 	var log = "# Upvoting " + sugId + " by "+userId + "...";
 
 	var sug = suggestions[sugId];
@@ -90,7 +109,7 @@ app.post("/votes/:suggestion", function(req, res) {
 // 11. and 18. Downvote a suggestion
 app.delete("/votes/:suggestion", function(req, res) {
     var sugId = req.params.suggestion; // suggestion id is encoded in the url path
-    var userId = 2; // for now
+    var userId = req.userId;
     var log = "# Downvoting " + sugId + " by " + userId + "...";
 
     var sug = suggestions[sugId];
